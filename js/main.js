fbb5beef68c03a920b1ef841eb6abfea650babc3
DariaIkon <ikonnikova.daria@gmail.com>
2022-02-02 21:37:37 +0300

    done

--- js/main.js
+++ js/main.js
@@ -1,17 +1,64 @@
 (function () {
     'use strict'
+
+    let loader = $('#loader');
     $('#order_form__button').click(function () {
-        let url = 'https://itlogia.ru/test/checkout?name=' + $('#order_input_name').val();
-        let http = new XMLHttpRequest();
-        http.open(method = 'POST', url);
-        http.send();
-        http.onreadystatechange = function () {
-            if (http.readyState === 4 && http.status === 200) {
-                console.log(http.responseText);
-            }
+        console.log('тык');
+        
+        $('.order_input_error').hide();
+        let name = $('#order_input_name');
+        let address = $('#order_input_address');
+        let phone = $('#order_input_phone');
+        let hasError = false;
+        let orderInput = $('.order_form_control');
+        let orderForm = $('#order_form')
+
+        if (!name.val()) {
+            name.siblings('.order_input_error').show();
+             orderInput.css({
+                'border': '1px solid red',
+                'margin-bottom': '5px'
+            })
+            hasError = true;
         }
-        // $('#order_form__button').click(function () {
-        //     console.log('тык');
-        // })
-    });
+        if (!address.val()) {
+            address.siblings('.order_input_error').show();
+             orderInput.css({
+                'border': '1px solid red',
+                'margin-bottom': '5px'
+            })
+            hasError = true;
+        }
+        if (!phone.val()) {
+            phone.siblings('.order_input_error').show();
+            orderInput.css({
+                'border': '1px solid red',
+                'margin-bottom': '5px'
+            });
+            hasError = true;
+        }
+        if (!hasError) {
+            loader.css('display', 'flex');
+             $.ajax({
+        method: "POST",
+        url: 'https://itlogia.ru/test/checkout',
+        data: {name: name.val(), address: address.val(), phone: phone.val()}
+        })
+                 .done(function (msg) {
+             loader.hide();
+            console.log(msg);
+            if (msg.success) {
+                orderForm.css('display', 'none');
+                orderForm.siblings('#order_form_success ').css('display', 'flex');
+            } else {
+                alert('Возникла ошибка при оформлении заказа, позвоните нам и сделайте заказ');
+            }
+        });
+        }
+
+       
+
+    })
+        
+    
 }());
